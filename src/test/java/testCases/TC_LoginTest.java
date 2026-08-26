2f1486d5a544e41111b4e72473e842d9735a75dd
Dhanaji42 <dhanaji424242@gmail.com>
2026-08-26 18:08:58 +0530

    test

--- src/test/java/testCases/TC_LoginTest.java
+++ src/test/java/testCases/TC_LoginTest.java
@@ -1,38 +1,38 @@
-package testCases;
-
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-import com.aventstack.extentreports.ExtentTest;
-
-import factory.BaseClass;
-import Pages.LoginPageTest;
-
-public class TC_LoginTest extends BaseClass {
-
-    @Test
-    public void verifyLogin() {
-
-        ExtentTest test = extent.createTest("Verify Login");
-
-        LoginPageTest lp = new LoginPageTest(driver);
-
-        test.info("Entering Username");
-        lp.enterUserName("tomsmith");
-
-        test.info("Entering Password");
-        lp.enterPassword("SuperSecretPassword!");
-
-        test.info("Clicking Login Button");
-        lp.clickLogin();
-
-        String actualMessage = lp.getSuccessMessage();
-
-        Assert.assertTrue(
-                actualMessage.contains("You logged into a secure area!"),
-                "Login Failed"
-        );
-
-        test.pass("Login Successful");
-    }
-}
+//package testCases;
+//
+//import org.testng.Assert;
+//import org.testng.annotations.Test;
+//
+//import com.aventstack.extentreports.ExtentTest;
+//
+//import factory.BaseClass;
+//import Pages.LoginPageTest;
+//
+//public class TC_LoginTest extends BaseClass {
+//
+//    @Test
+//    public void verifyLogin() {
+//
+//        ExtentTest test = extent.createTest("Verify Login");
+//
+//        LoginPageTest lp = new LoginPageTest(driver);
+//
+//        test.info("Entering Username");
+//        lp.enterUserName("tomsmith");
+//
+//        test.info("Entering Password");
+//        lp.enterPassword("SuperSecretPassword!");
+//
+//        test.info("Clicking Login Button");
+//        lp.clickLogin();
+//
+//        String actualMessage = lp.getSuccessMessage();
+//
+//        Assert.assertTrue(
+//                actualMessage.contains("You logged into a secure area!"),
+//                "Login Failed"
+//        );
+//
+//        test.pass("Login Successful");
+//    }
+//}
